eed1201bad22abdc9388b30582a3755dae524fce
Daniel Ramos <danielramosacosta@hotmail.com>
2020-05-10 02:19:54 +0100

    Refactor named handling

--- src/container.ts
+++ src/container.ts
@@ -4,20 +4,20 @@ type ClassOf<T> = new (...args: any[]) => T
 
 export class Container {
   private mapper = new Map<symbol, any>()
-  private mapperConstraints = new Map<symbol, any>()
 
   bind<T>(serviceIdentifier: symbol) {
     return {
       to: (TheClass: ClassOf<T>) => {
-        this.mapper.set(serviceIdentifier, TheClass)
+        if (!this.mapper.get(serviceIdentifier)) {
+          this.mapper.set(serviceIdentifier, TheClass)
+        }
 
         return {
           whenTargetNamed: (target: symbol | string) => {
-            if (!(this.mapperConstraints.get(serviceIdentifier) instanceof Map)) {
-              this.mapperConstraints.set(serviceIdentifier, new Map())
+            if (!(this.mapper.get(serviceIdentifier) instanceof Map)) {
+              this.mapper.set(serviceIdentifier, new Map())
             }
-
-            this.mapperConstraints.get(serviceIdentifier).set(target, TheClass)
+            this.mapper.get(serviceIdentifier).set(target, TheClass)
           },
           whenParentNamed: (target: string) => {},
         }
@@ -29,7 +29,7 @@ export class Container {
     let TargetClass = this.mapper.get(serviceIdentifier)
 
     if (tag) {
-      TargetClass = this.mapperConstraints.get(serviceIdentifier).get(tag)
+      TargetClass = this.mapper.get(serviceIdentifier).get(tag)
     }
 
     const dependencies = TargetClass[PARAMETERS_SYMBOLS_KEY] as symbol[]
